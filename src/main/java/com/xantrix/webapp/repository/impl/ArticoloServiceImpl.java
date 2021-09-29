06606fbd681e042dd5f051eba1b59681e038bce0
wolf1193 <roberto.amato.wolf.11.93@gmail.com>
2021-09-29 19:08:45 +0200

    aggiunt annotaion @Service

--- src/main/java/com/xantrix/webapp/repository/impl/ArticoloServiceImpl.java
+++ src/main/java/com/xantrix/webapp/repository/impl/ArticoloServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import com.xantrix.webapp.entity.Articolo;
 import com.xantrix.webapp.repository.ArticoloRepository;
 import com.xantrix.webapp.service.ArticoloService;
 
+@Service
 public class ArticoloServiceImpl implements ArticoloService {
 	private final Logger LOG = LoggerFactory.getLogger(ArticoloServiceImpl.class);
 	
